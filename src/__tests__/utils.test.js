6e301a81f506c302772e8866dcbbb3ea60c82e4e
aidadieudonne <aidaharing10@gmail.com>
2025-03-03 17:37:02 -0700

    passed second test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,6 +1,14 @@
 import { pointsForWord } from "../utils";
 
 describe("pointsForWord", () => {
+    it("handles uppercase and lowercase input", () => {
+        const word = "tEsT";
+        
+        const points = pointsForWord(word);
+
+        expect(points).toBe(7);
+    }); 
+    
     it("calculates the total points for a word (1 point per vowel, 2 points for consonant)", () => {
         const word = "test";
 
